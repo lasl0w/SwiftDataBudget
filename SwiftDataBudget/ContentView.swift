da960f1217757b972c7050028ab3be2071c5c4a4
Tom Montgomery <montgomery3681@gmail.com>
2024-04-09 22:10:51 -0600

    Initial Commit

---
+++ SwiftDataBudget/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftDataBudget
+//
+//  Created by tom montgomery on 4/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftDataBudget/SwiftDataBudgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataBudgetApp.swift
+//  SwiftDataBudget
+//
+//  Created by tom montgomery on 4/9/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataBudgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
